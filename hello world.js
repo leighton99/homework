ceea096d3a41841e04f4e6c83595edac03210d1a
leighton99 <leightonlee99@gmail.com>
2017-07-12 22:31:14 -0700

    javascript homework

---
+++ hello world.js
@@ -0,0 +1,181 @@
+
+// alert("Hello WOrld")
+
+
+// var x = 10;
+// var y = 20;
+
+// alert(x+y)
+
+// var leighton = {firstName:'leighton', lastName: 'lee', birthday: 'June'};
+
+// console.log(leighton.lastName);
+
+
+
+// var x = ["a", "b", "c"]
+// var x = ["e", "f", "g"]
+// var z = [x,y];
+// console.log ([0][2])
+
+
+
+// if (20>10) {
+
+// 	console.log("false")
+// }
+
+// var y = 10;
+// var x = 5; 
+
+// if (y < x) {
+
+// 	console.log("false")
+
+// } else {console.log("true")
+
+// }
+
+// function alertName(randomname){
+// 	return alert(randomname ==="leighton");
+
+// }
+
+// alertName("david"); false 
+// alertName("leighton"); true
+
+// function door(lee){
+
+// 	if(lee===1){
+// 		alert("won");
+// 	}else if(lee===2){
+// 		alert("lost");
+// 	}else if(lee===3){
+// 		alert("lostagain");
+// 	}else {
+// 		alert("gameover");
+// 	}
+// }
+
+// door(1);
+
+
+// var names = ["leighton", "david", "lee", "john", "luffy"]
+// for (var i = 0; i< names.length; i++){
+// 	console.log(names[i])
+// }
+
+// var x = 99
+// 	while (x > ){
+// 		console.log('99 Bottles of Root Beer on the Wall' + x-1)
+// 		x ++;
+// 	}
+	
+// // sings = ["99 Bottles of Root Beer on the Wall"]
+// // 	while(sings < ){
+// // 		console.log('' + sings)
+// // 		sings++;
+// // 	}
+
+
+// var x = 2
+
+// if(x === 3) {
+//   console.log("sushi is tasty");
+// } else if (x > 0) {
+//   console.log("sushi is delicious");
+// }
+
+// var food = "walnut"
+
+// if (food === "walnut"){
+//   console.log("i love walnuts");
+// }
+
+// var turtles = ["Leonardo", "Raphael", "Michelangelo", "Donatello"];
+// var a = turtles.indexOf ("Michelangelo")
+// console.log ('Michelangelo')
+
+// var turtles = ["Leonardo", "Raphael", "Michelangelo", "Donatello"]
+
+// for(i = 0; i < 2; i++){
+//   console.log(turtles[i]);
+// }
+
+// var numbers = ['6', '5', '4', '3', '2', '1'];
+// 	for (var numbers = -1; i < numbers.length; numbers--) {
+// 	 	numbers[i]
+// 	console.log('more is better')
+// }
+// else 
+
+
+
+
+// Assignment 1
+var lastName = 1;
+var firstName = 2;
+console.log(lastName + firstName);
+//
+
+// Assignment 2
+var lastName = "leighton";
+var firstName = "lee";
+console.log(firstName +" "+ lastName);
+//
+
+// Assignment 3
+var nameArray = [1,2,3,4,5,6,7,8];
+console.log(nameArray[0]);
+console.log(nameArray[5]);
+//
+
+// Assignment 3
+var x = 1;
+var y = 4;
+if(x < y) {
+  alert("variable is greater than 100");
+} else if (x > y) {
+  alert("variable is less than 100");
+}
+//
+
+// Assignment 5
+var a = 3;
+var b = 3;
+if (a==b){
+	alert("comparison is true");
+}
+//
+
+// Assignment 6
+function food (){
+	alert ("you have called this function")
+}
+
+food ();
+//
+
+// Assignment 7
+function sum (a, b){
+	if(a==="");
+}
+sum ("");
+//
+
+// Assignment 8
+function door(lee){
+
+	if(lee===1){
+		alert("won");
+	}else if(lee===2){
+		alert("lost");
+	}else if(lee===3){
+		alert("lostagain");
+	}else {
+		alert("gameover");
+	}
+}
+
+door(6);
+//
